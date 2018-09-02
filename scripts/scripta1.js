c7eccb043e0a11fdfb792148547e0839edd1e29c
IgorJercic <igzyy.j@gmail.com>
2018-09-02 12:39:43 +0200

    start
    samo start

---
+++ scripts/scripta1.js
@@ -0,0 +1,18 @@
+var myApp = angular.module("myModul", []).controller("myController", function($scope){
+
+
+var employee = {
+
+ime: "igor",
+prezime: "jerčic",
+ulica : "nmegdhe",
+broj : 122
+};
+
+$scope.employee1 = employee;
+
+
+
+
+
+})
